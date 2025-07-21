2e9e2ee9440c0500dc944e23d3c3c55a02017424
Bhuvan <bhuvibhuvan1624@gmail.com>
2025-07-21 19:51:16 +0530

    FE: Completed All Pages and Ready for Deployment

--- frontend/src/Components/NoteCard.jsx
+++ frontend/src/Components/NoteCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import { PenSquareIcon, Trash, Trash2Icon } from 'lucide-react'
 import { Link } from 'react-router'
-import { formatDate } from '../Utils/utils'
+import { formatDate } from '../Libs/utils'
+import api from '../Libs/axios.js'
+import { toast } from 'react-hot-toast'
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault();//To prevent from navigating to NoteDetailPage
+
+        if (!window.confirm("Are you sure want to delete this Note")) return;
+        try {
+            await api.delete(`/notes/${id}`)
+            setNotes((prev) => prev.filter(note => note._id !== id))//Get rid of delete one from array
+            toast.success("Note successfully Deleted")
+        } catch (error) {
+            console.log("Error in Deleting Note");
+            toast.error("Failed to delete Note")
+
+        }
+
+
+    }
     return <>
         <Link to={`/note/${note._id}`}
             className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#41a780]'>
@@ -14,9 +33,9 @@ const NoteCard = ({ note }) => {
                     <span className='text-small text-base-content/60'>
                         {formatDate(new Date(note.createdAt))}
                     </span>
-                    <div className='flex items-center gap-1'>
+                    <div className='flex items-center  gap-1'>
                         <PenSquareIcon className='size-4' />
-                        <button className="btn btn-ghost btn-xs text-error">
+                        <button onClick={(e) => handleDelete(e, note._id)} className="btn btn-ghost btn-xs text-error">
                             <Trash2Icon className="size-4" />
                         </button>
 
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import axios from 'axios'
 import toast from 'react-hot-toast';
 import React, { useEffect, useState } from 'react'
 import NavBar from '../Components/NavBar'
 import RateLimitedUi from '../Components/RateLimitedUi';
 import NoteCard from '../Components/NoteCard';
+import api from '../Libs/axios.js'
+import { PlusIcon } from 'lucide-react';
+import { Link } from "react-router"
+import NoteNotFound from '../Components/NoteNotFound.jsx';
 
 
 const HomePage = () => {
@@ -16,10 +19,10 @@ const HomePage = () => {
 
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes")
+        const res = await api.get("notes")
         setIsRateLimited(false);
         setNotes(res.data.notes);
-        console.log("This is Notes after fetching",notes);
+        console.log("This is Notes after fetching", notes);
       } catch (error) {
         console.log('Error fetching data')
         console.log(error.response)
@@ -42,16 +45,17 @@ const HomePage = () => {
 
   return (
     <div className='min-h-screen'>
-      <NavBar />
+      <NavBar notes={notes} />
       {isRateLimited && <RateLimitedUi />}
 
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
+      <div className='max-w-7xl mx-auto p-4 mt-6 '>
         {loading && <div className='text-center text-primary py-10'>Loading...</div>}
+        {notes.length === 0 && !isRateLimited && !loading && (<NoteNotFound />)}
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map((item) =>
             (
-              <NoteCard key={item._id} note={item}/>
+              <NoteCard key={item._id} note={item} setNotes={setNotes} />
             )
             )}
 
